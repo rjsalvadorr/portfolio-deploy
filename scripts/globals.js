6019f86394f88b6149bf82132eb121e0f512357b
RJ Salvador <rjsalvadorr@gmail.com>
2019-03-24 20:52:45 -0400

    Site updated: 2019-03-24 20:52:37

--- scripts/globals.js
+++ scripts/globals.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
      $('.content-tile-img > a').litebox();
      $('.nav-link--mobile').click(function () {
         $('.hidden-link-container').toggleClass('active');
+        $('.menu-link-container').toggleClass('active');
         $('.nav-link--mobile').toggleClass('active');
      });
 });
